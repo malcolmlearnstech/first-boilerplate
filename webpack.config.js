16f9aa630e2191c9cfadf875a0c0e7a512c3106b
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-02-16 22:23:11 -0500

    fix : edits to babel presets in webpack configuration file

--- webpack.config.js
+++ webpack.config.js
@@ -9,11 +9,16 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
-        exclude: /node_modules/,
+        exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-react'],
+          presets: ['@babel/preset-react', '@babel/preset-env'],
         },
+      },
+      // the following uses style-loader/css-loader combos for anything matching the .css extension
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
