b1e84c0048f31552be44858799903a89b481804f
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-02-16 22:21:29 -0500

    connected stylesheet to client index.js and added style to page

--- client/index.js
+++ client/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM, { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import './index.css';
 
 // make sure this is the same as the id of the div in your index.html
 ReactDOM.createRoot(document.getElementById('app')).render(
   <Provider store={store}>
-    <h2>
-      <div>Hello, world!</div>
-    </h2>
+    <div className="indexStyling">
+      <h2>Hello, world!</h2>
+    </div>
   </Provider>
 );
