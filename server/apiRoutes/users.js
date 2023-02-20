de0de17ced280e314a8cd50eab55c0463e628e1d
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-02-19 22:43:04 -0500

    added directions for where to place Users backend routes

--- server/apiRoutes/users.js
+++ server/apiRoutes/users.js
@@ -2,15 +2,39 @@
 const router = require('express').Router();
 
 // matches GET requests to /api/users/
-router.get('/', function (req, res, next) {});
+router.get('/', function (req, res, next) {
+  /**
+   *
+   * write out route for GET request
+   *
+   */
+});
 
 // matches POST requests to /api/users/
-router.post('/', function (req, res, next) {});
+router.post('/', function (req, res, next) {
+  /**
+   *
+   * write out route for POST request
+   *
+   */
+});
 
-// matches PUT requests to /api/tasks/:taskId
-router.put('/:userId', function (req, res, next) {});
+// matches PUT requests to /api/users/:UserId
+router.put('/:userId', function (req, res, next) {
+  /**
+   *
+   * write out route for PUT request
+   *
+   */
+});
 
-// matches DELETE requests to /api/tasks/:taskId
-router.delete('/:userId', function (req, res, next) {});
+// matches DELETE requests to /api/users/:UserId
+router.delete('/:userId', function (req, res, next) {
+  /**
+   *
+   * write out route for DELETE request
+   *
+   */
+});
 
 module.exports = router;
