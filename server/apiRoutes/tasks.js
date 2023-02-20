52ab2849d4c58e87d722c4a8e5eef613b55d67a3
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-02-19 22:42:28 -0500

    added directions for where to place Tasks backend routes

--- server/apiRoutes/tasks.js
+++ server/apiRoutes/tasks.js
@@ -2,15 +2,39 @@
 const router = require('express').Router();
 
 // matches GET requests to /api/tasks/
-router.get('/', function (req, res, next) {});
+router.get('/', function (req, res, next) {
+  /**
+   *
+   * write out route for GET request HERE
+   *
+   */
+});
 
 // matches POST requests to /api/tasks/
-router.post('/', function (req, res, next) {});
+router.post('/', function (req, res, next) {
+  /**
+   *
+   * write out route for POST request HERE
+   *
+   */
+});
 
 // matches PUT requests to /api/tasks/:taskId
-router.put('/:taskId', function (req, res, next) {});
+router.put('/:taskId', function (req, res, next) {
+  /**
+   *
+   * write out route for PUT request HERE
+   *
+   */
+});
 
 // matches DELETE requests to /api/tasks/:taskId
-router.delete('/:taskId', function (req, res, next) {});
+router.delete('/:taskId', function (req, res, next) {
+  /**
+   *
+   * write out route for DELETE request HERE
+   *
+   */
+});
 
 module.exports = router;
