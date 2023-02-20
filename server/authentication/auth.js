7bfee31f2cf735508efc687b875a884fa3ba3bcf
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-02-19 22:45:04 -0500

    created page for User authentication routes

---
+++ server/authentication/auth.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+import User from '../db/models/user';
+
+//this would be our login route
+router.post('/login', async (req, res, next) => {
+  try {
+    res.send({ token: await User.authenticate(req.body) });
+  } catch (err) {
+    next(err);
+  }
+});
+
+//this would be the signup route
+router.post('/signup', async (req, res, next) => {
+  try {
+    const user = await User.create(req.body);
+    res.send({ token: await user.generateToken() });
+  } catch (err) {
+    if (err.name === 'SequelizeUniqueConstraintError') {
+      res.status(401).send('User already exists');
+    } else {
+      next(err);
+    }
+  }
+});
+
+module.exports = Auth;
