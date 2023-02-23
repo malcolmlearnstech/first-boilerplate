a8f65f7bd1bf1ff97e443e7438ca8f59065abae5
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-02-22 23:16:35 -0500

    edit descriptions of router use in apiRoutes serve up file

--- server/apiRoutes/index.js
+++ server/apiRoutes/index.js
@@ -3,8 +3,8 @@ const router = require('express').Router();
 //router.use('/login', require('../authentication/auth'))//middleware for authenticating login HERE
 //router.use('/signup', require('../authentication/auth'))//middleware for authenticating signup HERE
 
-router.use('/users', require('./users')); //matches all requests to /api/users/
-router.use('/tasks', require('./tasks')); //matches all requests to  /api/tasks/
+router.use('/users', require('./users')); //matches all user related requests to /api/users/
+router.use('/tasks', require('./tasks')); //matches all task related requests to /api/tasks/
 
 //error handling for an API route that does not exist
 router.use(function (req, res, next) {
